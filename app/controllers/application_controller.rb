deb6396a4753227453da9f6059bb2783b719780d
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-05-09 10:41:02 +0900

    ログイン機能ログイン後画面ヘッダー設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+	before_action :authenticate_user!
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	#サインインした後の画面
+	def after_sign_in_path_for(resource)
+		new_book_path
+	end
+	def after_sign_out_path_for(resource)
+	    root_path # ログアウト後に遷移するpathを設定
+    end
+	protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root 'home#top'
+	get 'home/about', to: 'home#about'
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show]
+  # For details on the DSL  within this file, see http://guides.rubyonrails.org/routing.html
 end

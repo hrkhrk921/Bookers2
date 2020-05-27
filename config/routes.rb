5dacf5f74956580eb16669c2dc486670afa12cab
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-05-27 22:40:33 +0900

    bundle update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-	root 'home#top'
+	root to: 'home#top'
 	get 'home/about', to: 'home#about'
 	get '/users', to: 'users#index'
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
   resources :users
   # For details on the DSL  within this file, see http://guides.rubyonrails.org/routing.html
+end
 end
